4e557b42e51a6968a02ef0382b3093c4fb0aedd1
julio alberto perez <piyoyo279@gmail.com>
2024-11-02 22:17:22 -0400

    banco

--- api/conex.php
+++ api/conex.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = " phpMyAdmin demo - MySQL"; 
-$user = "root"; 
-$pass = "";
-$db = "PRUEBA"; 
+$server = "sql.freedb.tech"; 
+$user = "freedb_julio01"; 
+$pass = 'D$zA$kNPsf2S59j';
+$db = "freedb_pulperia1"; 
 
 $con = mysqli_connect($server,$user,$pass,$db) or die("Problemas de conexion");
 ?>
