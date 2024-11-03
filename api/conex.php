dd9d0190f1576921941d83698bbee4fa48a6f3b9
julio alberto perez <piyoyo279@gmail.com>
2024-11-02 20:24:17 -0400

    bancos

--- api/conex.php
+++ api/conex.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "sql113.byetcluster.com"; 
-$user = "if0_36498161"; 
-$pass = "9SlOQr6yvL7pG9";
-$db = "if0_36498161_pulperia1"; 
+$server = " phpMyAdmin demo - MySQL"; 
+$user = "root"; 
+$pass = "";
+$db = "PRUEBA"; 
 
 $con = mysqli_connect($server,$user,$pass,$db) or die("Problemas de conexion");
 ?>
